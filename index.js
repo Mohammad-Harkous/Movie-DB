1b160e09a94074d86da5318a6f6bdbb0ccaca973
Mohammad-Harkous <mohammad.a.harkous@gmail.com>
2022-12-27 01:08:26 +0200

    step 2

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require('express');
 // express app
 const app = express();
 
-// list for requests
-app.listen(3000);
 
 app.get('/', function(req, res){
     res.send('ok')
 })
+
+// list for requests
+app.listen(3000);

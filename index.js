ba2fc993f7342b8b6bba035b87d622987c7dc846
Mohammad-Harkous <mohammad.a.harkous@gmail.com>
2022-12-27 01:46:28 +0200

    step 3

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ const express = require('express');
 // express app
 const app = express();
 
+const time = new Date();
+
+let hours = time.getHours();
+let minutes = time.getMinutes();
+
+let currentTime = `${hours}:${minutes}`
+
+
 
 app.get('/', function(req, res){
     res.send('ok')
+})
+
+app.get('/test', function(req, res){
+    res.status(200).send({status:200, message:"ok"})
+})
+
+app.get('/time', function(req, res){
+    res.status(200).send({status:200, message: currentTime})
 })
 
 // list for requests

1e998aa7ca9ff354d4ca3808066a48dd81f1aa72
Mohammad-Harkous <mohammad.a.harkous@gmail.com>
2022-12-29 03:36:20 +0200

    step 11

--- index.js
+++ index.js
@@ -164,5 +164,105 @@ app.put('/movies/update/:id', (req, res) => {
     }
 })
 
+// Step 11 - Use HTTP Verbs
+
+// get request for  moives by its date and ordered by date
+app.get('/movies/read/by-date', (req, res) => {
+
+    const date =   movies.sort(function(a,b){
+        return new Date(b.year) - new Date(a.year);
+      });
+   
+    res.status(200).send({status:200, data:date})
+})
+
+// get request for moives by its rating and ordered by rating
+app.get('/movies/read/by-rating', (req, res) => {
+    const rate =   movies.sort(function(a,b){
+        return (b.rating) - (a.rating);
+      });
+
+    res.status(200).send({status:200, data: rate})
+})
+
+// get request for moives by its title and ordered by title
+app.get('/movies/read/by-title', (req, res) => {
+    const title = movies.sort(function(a, b) {
+        const titleA = a.title.toUpperCase();
+        const titleB = b.title.toUpperCase();
+        return (titleA < titleB) ? -1 : (titleA > titleB) ? 1 : 0;
+    });
+
+    res.status(200).send({status:200, data: title})
+})
+
+// get request search for a moive by its id
+app.get('/movies/read/id/:id', (req,res) => {
+
+    const id = req.params.id
+    const found = movies.some(movie => movie.id === parseInt(id))
+
+    if (found) {
+      res.status(200).json(movies.filter(movie => movie.id === parseInt(id)))
+    }else{
+        res.status(404).json({status:404, error:true, message:'the movie of id ' + id + ' does not exist'})
+    }
+})
+
+// post request for creating new movie
+app.post('/movies/create', (req, res) => {
+    const addMovie = {
+       id: req.body.id,
+       title: req.body.title,
+       year: req.body.year,
+       rating:req.body.rating
+    }
+   
+    if(!addMovie.id || !addMovie.title || !addMovie.year || !Number.isInteger(addMovie.year) || addMovie.year <= 4 ) {
+       return res.status(403).send({status:403, error:true, message:'you cannot create a movie without providing a title and a year'})
+    }else if(addMovie.rating == null){
+       addMovie.rating = 4
+    }
+   
+    movies.push(addMovie)
+   
+    res.send("movie added successfully")
+   })
+
+
+// delete request  for deleting a moive by its id
+app.delete('/movies/delete/:id', (req, res) => {
+    const id = req.params.id
+    const found = movies.some(movie => movie.id === parseInt(id))
+
+    if (found) {
+      res.status(200).json({ message: 'movie was deleted', movies:movies.filter(movie => movie.id !== parseInt(id))})
+    }else{
+        res.status(404).json({status:404, error:true, message:'the movie of id ' + id + ' does not exist'})
+    }
+})
+
+// update request for updating a moive by its id
+app.put('/movies/update/:id', (req, res) => {
+    const id = req.params.id
+    const found = movies.some(movie => movie.id === parseInt(id))
+
+    if (found) {
+        const updateMovie = req.body
+        movies.forEach(movie => {
+            if(movie.id === parseInt(id)){
+                movie.title = updateMovie.title ? updateMovie.title : updateMovie.title
+                movie.year = updateMovie.year ? updateMovie.year : updateMovie.year
+                movie.rating = updateMovie.rating ? updateMovie.rating : updateMovie.rating
+
+                res.json({message: 'movie update', movie: movie})
+            }
+        })
+    }else{
+        res.status(404).json({status:404, error:true, message:'the movie of id ' + id + ' does not exist'})
+    }
+})
+
+
 // list for requests
 app.listen(3000);
